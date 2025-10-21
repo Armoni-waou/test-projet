665828829561a927670debffdcb2860f8965a49e
Armoni-waou <romaindu22560@gmail.com>
2025-10-21 09:31:25 +0200

    Correct syntax and create DataFrame from lists
    Fix syntax errors and create a DataFrame from lists.

--- app.py
+++ app.py
@@ -2,4 +2,9 @@ import streamlit as st
 
 st.header("mon app")
 
+nombres = [1, 2, 3, 4, 5]
+carré = [1**2, 2**2, 3**2, 4**2, 5**2]
+d = {"nombres", nombres "carré":carré}
+data = pd.DataFrame(d)
+
 st.write("Romain")
