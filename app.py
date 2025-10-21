e03bb81bed0fa4b879465d3841f7fc0697f8f215
Armoni-waou <romaindu22560@gmail.com>
2025-10-21 09:32:37 +0200

    Fix dictionary syntax in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.header("mon app")
 
 nombres = [1, 2, 3, 4, 5]
 carré = [1**2, 2**2, 3**2, 4**2, 5**2]
-d = {"nombres", nombres "carré":carré}
+d = {"nombres":nombres, "carré":carré}
 data = pd.DataFrame(d)
 
 st.write("Romain")
