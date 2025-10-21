2b5fa337101b6c8b295e05b1e67f8486f315221a
Armoni-waou <romaindu22560@gmail.com>
2025-10-21 09:43:38 +0200

    Revise headers and add demonstration in app.py
    Updated headers and added a demonstration section in the app.

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-st.header("mon app")
+st.header("L'appli de Cataclop aka Lise")
+
+st.header("Voici une petite demonstation de mes talents")
+
 
 nombres = [1, 2, 3, 4, 5]
 carré = [1**2, 2**2, 3**2, 4**2, 5**2]
 d = {"nombres":nombres, "carré":carré}
 data = pd.DataFrame(d)
+
+st.header("n'hesitez pas aller voir l'appli de Romain : 
+https://bonjourre-tousses.streamlit.app/")
+
+
+st.header("attention aux espions")
+st.header("Si vous vous sentez observé, ce qui va suivre vous serra utile")
 
 st.write(data)
